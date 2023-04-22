a707a4698a11ab7f6f30bb7e9115dc10d043265b
Xiangning2020 <61442496+Xiangning2020@users.noreply.github.com>
2023-04-21 22:22:06 -0400

    delete own function name map

--- driver.py
+++ driver.py
@@ -27,24 +27,47 @@ print("starting code!")
 top_n_function = {"do_syscall_64", "__GI___pthread_mutex_lock",  "lru_maintainer_thread",  "conn_cleanup", "syscall_return_via_sysret",  "ipt_do_table",  "nf_hook_slow", "ind_busiest_group","ipt_do_table","__fget", "drive_machine", "do_cache_free", "ixgbe_poll", "update_load_avg", "__pthread_mutex_unlock_usercnt"}
 # Write your code here....
 
-function_map = {}
-for event in perf_sample_events:
-    sample = event.sample_event
-    curr_sample_function = symbolize.get_symbols([sample.ip])[sample.ip]
-    if curr_sample_function in function_map:
-        function_map[curr_sample_function] = function_map[curr_sample_function] + 1
-    else:
-        function_map[curr_sample_function] = 1
+## with our own map:
+# function_map = {}
+# for event in perf_sample_events:
+#     sample = event.sample_event
+#     curr_sample_function = symbolize.get_symbols([sample.ip])[sample.ip]
+#     if curr_sample_function in function_map:
+#         function_map[curr_sample_function] = function_map[curr_sample_function] + 1
+#     else:
+#         function_map[curr_sample_function] = 1
 
-freq_counter = collections.Counter(function_map)
-top_functions = freq_counter.most_common(15)
+# freq_counter = collections.Counter(function_map)
+# top_functions = freq_counter.most_common(15)
+
+# top_functions_chains={}
+
+# for event in perf_sample_events:
+#     sample = event.sample_event
+#     curr_sample_function = symbolize.get_symbols([sample.ip])[sample.ip]
+#     if (curr_sample_function, freq in top_functions): ## only get the top 10 functions
+#         print("curr_sample_function name:")
+#         print(curr_sample_function)
+#         if curr_sample_function not in top_functions_chains:
+#             top_functions_chains[curr_sample_function] = []
+#         curr_chain = []
+#         for branch in sample.branch_stack:
+#             branch_from_ip = branch.from_ip
+#             branch_to_ip = branch.to_ip
+#             branch_from_symbol = symbolize.get_symbols([branch_from_ip])[branch_from_ip]
+#             # print("branch_from_symbol: ")
+#             # print(branch_from_symbol)
+#             curr_chain.append(branch_from_symbol)
+#         top_functions_chains[curr_sample_function].append(curr_chain)
+        
+# print(top_functions_chains)      
 
 top_functions_chains={}
 
 for event in perf_sample_events:
     sample = event.sample_event
     curr_sample_function = symbolize.get_symbols([sample.ip])[sample.ip]
-    if (curr_sample_function, freq in top_functions): ## only get the top 10 functions
+    if (curr_sample_function in top_n_function): ## only get the top 10 functions
         print("curr_sample_function name:")
         print(curr_sample_function)
         if curr_sample_function not in top_functions_chains:
@@ -59,4 +82,4 @@ for event in perf_sample_events:
             curr_chain.append(branch_from_symbol)
         top_functions_chains[curr_sample_function].append(curr_chain)
         
-print(top_functions_chains)      
+print(top_functions_chains) 
