4e5486fb5eb4c8136d5b47fb0e9c005848c137ce
root <root@node6.deepmish-191289.address-scaling-pg0.wisc.cloudlab.us>
2024-02-22 14:30:17 -0600

    cpu-cycles memcached

--- driver.py
+++ driver.py
@@ -154,58 +154,6 @@ def plot_tax_sharing_all_functions(perf_sample_events, ip_to_func_name):
     plt.cla()
     plt.clf()
 
-def plot_tax_sharing_top_functions(perf_sample_events, ip_to_func_name):
- 
-    # Initialize x-axis (tax categories) and y-axis (percentage of CPU cycles) data
-    xs = tax_categories
-    ys = [0 for _ in tax_categories]
-
-    # Calculate total CPU cycles across all samples and first branch stack
-    total_cpu_cycles = sum([event.sample_event.branch_stack[0].cycles for event in perf_sample_events if event.sample_event.branch_stack])
-
-    # Iterate over each sample event
-    for (i, event) in enumerate(perf_sample_events):
-        sample = event.sample_event
-        taxes_found = []
-
-        # Iterate over first branch stacks in each sample
-        if sample.branch_stack:
-            branch = sample.branch_stack[0]
-            instruction_pointer = branch.from_ip
-            function_name = ip_to_func_name.get(instruction_pointer, None)
-            cat = bucketize(function_name)
-
-            if(cat is None):
-                cat = "application_logic"
-
-            if cat not in taxes_found:
-                ys[xs.index(cat)] += (branch.cycles / total_cpu_cycles) * 100
-                # taxes_found.append(cat)
-
-    # Print raw data for tax sharing
-    print("Tax Sharing Raw Data for all functions")
-    print(xs)
-    print(ys)
-
-    # Sort tax categories based on percentage of CPU cycles for better visualization
-    xs = sorted(xs, key=(lambda x: ys[xs.index(x)]), reverse=True)
-    ys.sort(reverse=True)
-
-    # Create a bar plot
-    ax = sns.barplot(x=xs, y=ys, palette="muted")
-
-    # Customize plot appearance
-    plt.xticks(rotation=45, ha="right", rotation_mode="anchor")
-    plt.xlabel("Tax Categories", fontsize=16)
-    plt.ylabel("Percentage of CPU Cycles", fontsize=16)
-    plt.title("Memcached Tax Sharing Top functions", fontsize=16)
-    plt.savefig("cpu_cycles_memcached/tax_sharing_top_functions.png", bbox_inches="tight")
-
-    # Clear and close the plot to avoid overlapping with other plots
-    plt.cla()
-    plt.clf()
-
-
 
 def plot_all_branches_sample_attribution(perf_sample_events, ip_to_func_name):
 
@@ -261,7 +209,96 @@ def plot_all_branches_sample_attribution(perf_sample_events, ip_to_func_name):
     # Show the plot
     plt.show()
 
+# =============================================================================
+# Function: plot_tax_heatmap
+# Description: This function generates a heatmap representing the frequency of function calls between different tax categories in the Memcached application,
+#              across all samples, considering all branch stacks.
+# =============================================================================
 
+def plot_tax_heatmap(perf_sample_events, ip_to_func_name):
+
+   # Initialize list to store chains of tax categories for each branch stack
+    bucketized_chains = []
+
+     # Iterate over each sample event
+    for (i, event) in enumerate(perf_sample_events):
+        sample = event.sample_event
+        curr_chain = []
+
+        # Iterate over each branch in the branch stack
+        for branch in sample.branch_stack:
+            ip = branch.from_ip
+            func = ip_to_func_name[ip]
+
+            # Categorize the function into a tax category
+            if func is None or func == "":
+                cat = "application_logic"
+            else:
+                cat = bucketize(func)
+
+            curr_chain.append(cat)
+        bucketized_chains.append(curr_chain)
+
+    # Initialize arrays for heatmap data
+    heatmap_hops = np.full((len(tax_categories), len(tax_categories)), -1)
+    heatmap_annotation = np.zeros((len(tax_categories), len(tax_categories)))
+
+    # Calculate frequency of function calls between tax categories
+    for i, from_tax in enumerate(tax_categories):
+        for j, to_tax in enumerate(tax_categories):
+            if from_tax == to_tax:
+                continue
+            heat_val = 0
+            path_count = 0
+
+            # Iterate over each chain of tax categories
+            for chain in bucketized_chains:
+                min_hops = 33
+                found = False
+
+
+                # Find the minimum number of function calls between from_tax and to_tax in each chain
+                for (chain_idx, bucket) in enumerate(chain):
+                    if bucket == from_tax:
+                        to_chains = chain[chain_idx:]
+                        for (search_idx, curr_bucket) in enumerate(to_chains):
+                            if curr_bucket == to_tax:
+                                found = True
+                                min_hops = min(min_hops, np.abs(search_idx - chain_idx))
+
+                heat_val += min_hops if found else 0
+                path_count += 1 if found else 0
+
+            # Store frequency of function calls in heatmap data arrays
+            heatmap_annotation[i, j] = path_count
+            if path_count == 0:
+                continue
+            heatmap_hops[i, j] = heat_val / path_count
+
+    # Generate heatmap
+    ax = sns.heatmap(heatmap_hops,
+                     xticklabels=tax_categories,
+                     yticklabels=tax_categories,
+                     annot=heatmap_annotation,
+                     fmt="g",
+                     linewidths=1,
+                     linecolor='black',
+                     vmax=20,
+                     annot_kws={"size": 7},
+                     cbar={"label": "# Function Calls Between"})
+
+    # Customize plot appearance
+    plt.xticks(rotation=45, ha="right", rotation_mode="anchor")
+
+    # Add color bar label
+    cbar = ax.collections[0].colorbar
+    cbar.set_label("# Function Calls Between", size=9)
+
+    # Set plot title
+    plt.title("Memcached Tax Heatmap")
+
+    # Save the plot
+    plt.savefig("cpu_cycles_memcached/tax_heatmap.png", bbox_inches="tight")
 
 
 # =============================================================================
@@ -292,11 +329,9 @@ with open("ip_map.pickle", "wb") as f:
     # ip_to_func_name = pickle.load(f)
     # print(ip_to_func_name.keys())
 
-
-
 def work():
-    print("Plotting Tax Sharing Top Functions")
-    plot_tax_sharing_top_functions(perf_sample_events, ip_to_func_name)
+    print("Plotting Tax Heatmap")
+    plot_tax_heatmap(perf_sample_events, ip_to_func_name)
     print("Plotting Tax Sharing All Functions")
     plot_tax_sharing_all_functions(perf_sample_events, ip_to_func_name)
     print("Plotting All Branches Sample Attribution")
